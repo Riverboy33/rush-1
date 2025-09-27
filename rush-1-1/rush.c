6c2ce626dbc45ef1f109bcfc53a085cedd1236a0
Gregoire <gregoire.riviere@epitech.eu>
2025-09-27 16:22:49 +0200

    [ADD] rush

---
+++ rush-1-1/rush.c
@@ -0,0 +1,43 @@
+/*
+** EPITECH PROJECT, 2025
+** rush.c
+** File description:
+** test
+*/
+#include <unistd.h>
+
+void column(int x, int y)
+{
+    for (int i = 0; i < y - 2; i++){
+        if (x > 1)
+            write(1, "|", 1);
+        for (int j = 0; j < x - 2; j++)
+            write(1, " ", 1);
+        write(1, "|\n", 2);
+    }
+}
+
+void line(int x)
+{
+    write(1, "o", 1);
+    for (int i = 0; i < x - 2; i++)
+        write(1, "-", 1);
+    write(1, "o\n", 2);
+}
+
+void rush(int x, int y)
+{
+    if (x == 1 && y == 1)
+        write(1, "o\n", 2);
+    if (y == 1 && x != 1)
+        line(x);
+    else if (x == 1){
+        write(1, "o\n", 2);
+        column(x, y);
+        write(1, "o\n", 2);
+    } else {
+        line(x);
+        column(x, y);
+        line(x);
+    }
+}
