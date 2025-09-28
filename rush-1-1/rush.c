5a115e76c76a20633eff66dd3e83839bba9d3aa6
Gregoire <gregoire.riviere@epitech.eu>
2025-09-28 10:25:15 +0200

    [ADD] coding style

--- rush-1-1/rush.c
+++ rush-1-1/rush.c
@@ -6,46 +6,70 @@
 */
 #include <unistd.h>
 
-void column(int x, int y)
+void rush_bis_bis(int x, int y)
 {
-    for (int i = 0; i < y - 2; i++){
-        if (x > 1)
-            write(1, "|", 1);
-        for (int j = 0; j < x - 2; j++)
-            write(1, " ", 1);
-        write(1, "|\n", 2);
-    }
-}
+    int j;
+    int i;
 
-void line(int x)
-{
-    write(1, "o", 1);
-    for (int i = 0; i < x - 2; i++)
+    for (j = 0; j < x - 2; j++)
         write(1, "-", 1);
-    write(1, "o\n", 2);
+    write(1, "o", 1);
+    write(1, "\n", 1);
+    i = 0;
+    while (i < y - 2) {
+        write(1, "|", 1);
+        for (j = 0; j < x - 2; j++)
+            write(1, " ", 1);
+        write(1, "|", 1);
+        write(1, "\n", 1);
+        i++;
+    }
+    write(1, "o", 1);
+    for (j = 0; j < x - 2; j++)
+        write(1, "-", 1);
+    write(1, "o", 1);
 }
 
-void condition(int x, int y)
+void rush_bis(int x, int y)
 {
-    if (x == 1 && y == 1)
-        write(1, "o\n", 2);
-    if (y == 1 && x != 1)
-        line(x);
-    else if (x == 1){
-        write(1, "o\n", 2);
-        column(x, y);
-        write(1, "o\n", 2);
-    } else {
-        line(x);
-        column(x, y);
-        line(x);
+    int i;
+
+    if (x == 1) {
+        write(1, "o", 1);
+        write(1, "\n", 1);
+        i = 0;
+        while (i < y - 2) {
+            write(1, "|", 1);
+            write(1, "\n", 1);
+            i++;
+        }
+        write(1, "o", 1);
+        return;
     }
+    write(1, "o", 1);
+    rush_bis_bis(x, y);
 }
 
 void rush(int x, int y)
 {
-    if (x <= 0 || y <= 0)
-        write(1, "Invalid size\n", 13);
-    else
-        condition(x, y);
+    int i;
+    int j;
+
+    if (x <= 0 || y <= 0) {
+        write(2, "Invalid size\n", 13);
+        return;
+    }
+    if (x == 1 && y == 1) {
+        write(1, "o", 1);
+        return;
+    }
+    if (y == 1) {
+        write(1, "o", 1);
+        for (j = 0; j < x - 2; j++)
+            write(1, "-", 1);
+        if (x > 1)
+            write(1, "o", 1);
+        return;
+    }
+    rush_bis(x, y);
 }
